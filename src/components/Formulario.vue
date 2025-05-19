3f1d2ebdd167550c53a2195f6c126c42bb37d5f5
caruazu <gustavocaruazu@gmail.com>
2025-05-19 20:58:39 -0300

    configuração dos estados e estado do projeto

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,7 +2,7 @@
   <div class="box formulario">
     <div class="columns">
       <div
-        class="column is-8"
+        class="column is-5"
         role="form"
         aria-label="Formulário para criação de uma nova tarefa"
       >
@@ -13,6 +13,21 @@
           v-model="descricao"
         />
       </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto" >
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+  
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div class="column">
         <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
@@ -21,8 +36,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
 import Temporizador from './Temporizador.vue'
+import { computed, defineComponent } from "vue";
+import { key } from "@/store";
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "Formulário",
@@ -30,20 +47,32 @@ export default defineComponent({
   components: {
     Temporizador
   },
+
   data () {
     return {
-      descricao: ''
+      descricao: '',
+      idProjeto: ''
     }
   },
+
   methods: {
     finalizarTarefa (tempoDecorrido: number) : void {
       this.$emit('aoSalvarTarefa', {
         duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao
+        descricao: this.descricao,
+        projeto: this.projetos.find(projeto => projeto.id === this.idProjeto)
       })
       this.descricao = ''
     }
+  },
+
+  setup() {
+    const store = useStore(key)
+    return {
+      projetos: computed(() => store.state.projetos)
+    }
   }
+
 });
 </script>
 <style>
